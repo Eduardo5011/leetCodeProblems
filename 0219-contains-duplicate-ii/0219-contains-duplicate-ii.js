7f5acd324df3e20bb5545ba12933a42123705443
Eduardo Rosado <86026301+Eduardo5011@users.noreply.github.com>
2024-01-05 18:08:48 -0500

    Time: 113 ms (42.99%), Space: 63.9 MB (57.13%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+     let hashMap = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (hashMap.hasOwnProperty(num) && i - hashMap[num] <= k) {
+            return true;
+        }
+        hashMap[num] = i;
+    }
+
+    return false;
+};
