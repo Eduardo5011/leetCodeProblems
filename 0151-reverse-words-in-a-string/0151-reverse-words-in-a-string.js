667af5c3e7ccd7f8cfee1270381cb9cf395bc410
Eduardo Rosado <86026301+Eduardo5011@users.noreply.github.com>
2024-02-13 21:39:54 -0500

    Time: 52 ms (76.18%), Space: 50.3 MB (40.88%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let words = s.trim().split(/\s+/)
+
+    words.reverse()
+
+    return words.join(" ")
+};
